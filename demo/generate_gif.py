119e8c6ae7ae27d45492112e9a8c14a8f350870b
kmatzaro <kmatzaro@gmail.com>
2025-06-30 14:58:11 +0300

    new gif + reqs

--- demo/generate_gif.py
+++ demo/generate_gif.py
@@ -1,5 +1,5 @@
 from moviepy.editor import VideoFileClip
 
-clip = VideoFileClip("E:\\ad_adas_projects\\perception_autonomous_driving\\lane_detection_20250630_143354.mp4").subclip(0, 10)  # optional: crop first 10 sec
+clip = VideoFileClip("E:\\ad_adas_projects\\perception_autonomous_driving\\lane_detection_20250630_143354.mp4").subclip(30, 90)  # optional: crop first 10 sec
 clip = clip.resize(width=480)  # scale down for GIF size
-clip.write_gif("./lane_detection_demo.gif", fps=10)
+clip.write_gif(".\\perception_autonomous_driving\\demo\\lane_detection_demo.gif", fps=10)
