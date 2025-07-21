4b5d67824111eaf219fc4faf52577bd16992babe
kmatzaro <kmatzaro@gmail.com>
2025-07-21 19:32:10 +0300

    new demo

--- perception_ad/demo/generate_gif.py
+++ perception_ad/demo/generate_gif.py
@@ -1,5 +1,5 @@
 from moviepy.editor import VideoFileClip
 
-clip = VideoFileClip("E:\\ad_adas_projects\\perception_autonomous_driving\\lane_detection_20250711_102121.mp4").subclip(60, 112)  # optional: crop first 10 sec
+clip = VideoFileClip("E:\\ad_adas_projects\\perception_ad\\recordings\\lane_detection_20250720_191858.mp4").subclip(95, 130)  # optional: crop first 10 sec
 clip = clip.resize(width=480)  # scale down for GIF size
-clip.write_gif(".\\perception_autonomous_driving\\demo\\lane_detection_demo.gif", fps=10)
+clip.write_gif(".\\perception_ad\\demo\\lane_detection_demo.gif", fps=10)
